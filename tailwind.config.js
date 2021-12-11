074a52121d8d0441d64fee08d0920acbd103b555
Alex Anson <1alex.anson@gmail.com>
2021-12-11 16:59:16 -0500

    Added source sans 3 as our font (i don't love it, but i don't mind it.) and added colors for gradient background.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gradient: {
+          customPurple: "#6b1b68",
+          customRed: "#c4172e",
+          customOrange: "#cc560c",
+        },
+      },
+      fontFamily: {
+        sans: [
+          '"Source Sans 3"',
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          '"Noto Sans"',
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
+    },
   },
   variants: {
     extend: {},
