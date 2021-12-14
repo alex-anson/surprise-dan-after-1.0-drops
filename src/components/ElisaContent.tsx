904509247ba9bb7d6f6b6b47b27a1e451223d13a
Elisa Crescentini <elisa@ruggedsoftware.dev>
2021-12-14 10:32:28 +0100

    added ElisaContent

--- src/components/ElisaContent.tsx
+++ src/components/ElisaContent.tsx
@@ -1,3 +1,39 @@
 export default function ElisaContent() {
-  return <>{/* 🔥 */}</>;
+  return (
+    <div className="text-center leading-7">
+      <h2 className="text-lg font-semibold">
+        🎉 Stilt v1.0 DEPLOYED 🎉 Woohoo!
+      </h2>
+      <p>But, there's an important thing that I have to say now.</p>
+      <p>
+        <strong>THANK YOU</strong>
+      </p>
+      <p>
+        I cannot find another word to express my deep gratitude to you,
+        <br /> thank you seems so little for me!
+      </p>
+      <ul>
+        <li>⭐ Thank you to believe in me</li>
+        <li>
+          ⭐ Thank you for support me in frustration 🤬, bugs 🐛 and successes
+          🎉🎉
+        </li>
+        <li>
+          ⭐ Thank you for teach me 👨‍🏫 so much things and give me time to learn
+        </li>
+        <li>
+          ⭐ Thank you for giving me so much ✨ FUN CHALLENGES ✨ that makes me
+          grow!! 🚀{" "}
+        </li>
+        <li>
+          ⭐ Thank you for showing me everyday that the DREAM JOB IS NOT A LIE!
+          🤩
+        </li>
+        <li>⭐ Thank you for being such a good friend 👩🏼‍🤝‍🧑🏼</li>
+      </ul>
+      <p>
+        I never thought anyone would give me this chance so, again, THANK YOU
+      </p>
+    </div>
+  );
 }
