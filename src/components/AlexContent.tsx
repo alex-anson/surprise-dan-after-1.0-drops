27702aeecf2e847c233b6cff1362f75fb17b1d76
Alex Anson <1alex.anson@gmail.com>
2021-12-20 15:47:44 -0500

    finished my content :tada:

--- src/components/AlexContent.tsx
+++ src/components/AlexContent.tsx
@@ -4,15 +4,32 @@ export default function AlexContent() {
       <h2 className="text-lg font-semibold">
         AHHHHH!!! I can't believe we're here. Stilt v1.0 deployed ✅ 🎉
       </h2>
-      <p>I wanna take the time to thank you.</p>
-      <p>
+      <p className="mb-4">I wanna take the time to thank you.</p>
+      <p className="mb-2">
         I never thought I'd be here when I started learning JS a year ago. A
         good deal of my tech knowledge comes from being involved in this project
-        (and nearly all of my experience).{" "}
+        (and nearly all of my experience).
+      </p>
+      <p className="mb-2">Day after day, I thank my lucky stars. 🌟</p>
+      <p className="mb-2">
+        I've learned <span className="italic">so much</span> from you. I'm miles
+        ahead of where I would've been, had you not drafted me 😂
+      </p>
+      <p className="mb-2">
+        There's so many small details and huge concepts that I understand
+        because of you. You've allowed me to discover my love of typescript,
+        unit testing, and backend. <br /> 🧬 &nbsp; &nbsp; 🧪 &nbsp; &nbsp; 🔙
+        🔚
+      </p>
+      <p className="mb-4">
+        I can't thank you enough for always supporting me through the ups and
+        downs. I can't &ndash; and don't want to &ndash; imagine where I'd be if
+        I hadn't fought through the imposter syndrome and said yes to this
+        opportunity. I can't wait to continue this journey.
       </p>
       <p>
-        You've allowed me to discover my love of unit testing, typescript, and
-        backend. I cannot wait to continue this journey.
+        PS &mdash; thank you for having the patience of a saint. 🤣 <br />
+        🧡
       </p>
     </>
   );
